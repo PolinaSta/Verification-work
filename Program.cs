2f0121d151d0033bc5f0795132524846646a40cc
Polina <polstaver@yandex.by>
2022-07-24 22:21:52 +0300

    Begining

--- Program.cs
+++ Program.cs
@@ -3,3 +3,43 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+string[] arrayOne =  {"Be", "future", "or"};
+int countOne = MeaningLessThreeSimb(arrayOne);
+string[] resultOne =  ResultArray(arrayOne,countOne);
+
+void PrintArray(string[] array) 
+    {
+
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+                else Console.Write($"{array[i]}.");
+            }
+        Console.WriteLine();
+    }
+
+int MeaningLessThreeSimb(string[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3) count++;
+            }
+        return count;
+    }
+
+string[] ResultArray(string[] array, int count)
+    {
+        string[] result = new string[count];
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                    {
+                        result[result.Length - count] = array[i];
+                        count--;
+                    }
+            }
+        return result;
+    }
+
+PrintArray(resultOne);
