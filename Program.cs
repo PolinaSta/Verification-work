3684a4814149f01503c25c21bd4f9fcfd1364729
Polina <polstaver@yandex.by>
2022-07-25 15:35:31 +0300

    Fix

--- Program.cs
+++ Program.cs
@@ -3,21 +3,56 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-string[] arrayOne =  {"Be", "future", "or", "no"};
+string[] arrayOne = { "Be", "future", "or", "no" };
 int countOne = MeaningLessThreeSimb(arrayOne);
-string[] resultOne =  ResultArray(arrayOne,countOne);
+string[] resultOne = ResultArray(arrayOne, countOne);
 
-string[] arrayTwo = {"big", "future", "car", "wonderful"};
-int countTwo  = MeaningLessThreeSimb(arrayTwo);
-string[] resultTwo = ResultArray(arrayTwo,countTwo);
+string[] arrayTwo = { "big", "future", "car", "wonderful" };
+int countTwo = MeaningLessThreeSimb(arrayTwo);
+string[] resultTwo = ResultArray(arrayTwo, countTwo);
 
-string[] arrayThree = {"mother", "we", "new", "day"};
+string[] arrayThree = { "mother", "we", "new", "day" };
 int countThree = MeaningLessThreeSimb(arrayThree);
 string[] resultThree = ResultArray(arrayThree, countThree);
 
-string[] arrayFour = {"up", "one", "51468746", "owl"};
+string[] arrayFour = { "up", "one", "51468746", "owl" };
 int countFour = MeaningLessThreeSimb(arrayFour);
 string[] resultFour = ResultArray(arrayFour, countFour);
+
+void PrintArray(string[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
+    }
+    Console.WriteLine();
+}
+
+int MeaningLessThreeSimb(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] ResultArray(string[] array, int count)
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - count] = array[i];
+            count--;
+        }
+    }
+    return result;
+}
 
 PrintArray(arrayOne);
 PrintArray(arrayTwo);
@@ -25,42 +60,6 @@ PrintArray(arrayThree);
 PrintArray(arrayFour);
 
 Console.WriteLine();
-
-
-void PrintArray(string[] array) 
-    {
-
-        for (int i = 0; i < array.Length; i++)
-            {
-                if (i != array.Length - 1) Console.Write($"{array[i]}, ");
-                else Console.Write($"{array[i]}.");
-            }
-        Console.WriteLine();
-    }
-
-int MeaningLessThreeSimb(string[] array)
-    {
-        int count = 0;
-        for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i].Length <= 3) count++;
-            }
-        return count;
-    }
-
-string[] ResultArray(string[] array, int count)
-    {
-        string[] result = new string[count];
-        for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i].Length <= 3)
-                    {
-                        result[result.Length - count] = array[i];
-                        count--;
-                    }
-            }
-        return result;
-    }
 
 PrintArray(resultOne);
 PrintArray(resultTwo);
